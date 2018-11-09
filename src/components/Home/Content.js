8e4f1fd0931566fadfb8b9e03b3baea79194da79
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2018-11-09 14:33:44 -0700

    added firebase to app

--- src/components/Home/Content.js
+++ src/components/Home/Content.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Content = () => {
     return (
         <div>
-            <h1>"Company Phrase"</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In placerat egestas risus. Vestibulum dignissim nec libero nec congue. Aenean lacinia turpis vel justo fermentum pulvinar. Praesent dapibus velit vel mauris tincidunt, at commodo libero lobortis. Pellentesque nec est nunc. Curabitur finibus turpis ante. Proin tristique molestie elit at varius. Maecenas aliquet venenatis enim. Phasellus sit amet mi eu lectus maximus pretium in eu velit. Vestibulum volutpat, dui ac faucibus rutrum, enim diam rhoncus sem, maximus rhoncus sem eros et nisl.</p>
+            <h1>Under Construction</h1>
+            <p>Please come back soon :)</p>
         </div>
     )
 }
