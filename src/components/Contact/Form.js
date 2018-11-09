b9d2e55c78e5d4b598697a49ffa1bc12b4d4be31
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2018-11-09 15:33:14 -0700

    Adding fb hosting to app

--- src/components/Contact/Form.js
+++ src/components/Contact/Form.js
@@ -3,10 +3,22 @@ import React from 'react';
 const ContactForm = ({ submit, change }) => {
     return (
         <form onSubmit={submit}>
-            <input type="text" name="name" onChange={change} />
-            <input type="text" name="email" onChange={change} />
-            <input type="text" name="phoneNumber" onChange={change} />
-            <input type="text" name="message" onChange={change} />
+            <label>Name: 
+               <input type="text" name="name" onChange={change} />
+            </label>
+            {' '}
+            <label>Email:
+                <input type="text" name="email" onChange={change} />
+            </label>
+            {' '}
+            <label>Phone Number:
+                <input type="text" name="phoneNumber" onChange={change} />
+            </label>
+            {' '}
+            <label>Message:
+                <input type="text" name="message" onChange={change} />
+            </label>
+            {' '}
             <button>Contact Us</button>
         </form>
     )
