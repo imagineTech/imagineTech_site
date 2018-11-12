e6cb5b0710d26789d44cc61ad4d8e1d4158392d8
Shubham Tiwari <websbiz0000@gmail.com>
2018-11-12 14:35:56 +0000

    replaces under cons. with launching jan 1st, 2019

--- src/components/Home/Content/Construction.js
+++ src/components/Home/Content/Construction.js
@@ -4,7 +4,7 @@ import HomeCountdown from '../../Countdown';
 const Construction = () => {
     return(
         <div className="col-sm-12">
-            <h2>Under Construction</h2>
+            <h2>Launching January 1st, 2019</h2>
             <p>ImagineTech team is working very hard on the new version of our site. It will bring a lot of new features. Stay tuned!</p>
             <HomeCountdown/>
         </div>
