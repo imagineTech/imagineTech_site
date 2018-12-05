b9690f77e600e0141e3e7a7bb8d63d1508c2da2b
Shubham Tiwari <websbiz0000@gmail.com>
2018-12-05 11:30:17 +0000

    bad changes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import { render } from 'react-dom';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history'
 import Root from './screens/Root';
-
+import '../node_modules/animate.css/animate.min.css';
 const history = createBrowserHistory();
 
 render(
     <Router history={history}>
         <Root />
-    </Router>, 
-document.getElementById('root'));
+    </Router>,
+    document.getElementById('root'));
--- src/repeats/Header.js
+++ src/repeats/Header.js
@@ -1,19 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
-    return (
-        <header>
+class Header extends React.Component {
+    state = {
+        hamburger: false
+    }
+    handleToggle = () => {
+        this.setState(state => ({ hamburger: !state.hamburger }))
+    }
+    render() {
+        let { hamburger } = this.state;
+        return (
+            <div className="top">
+            <header>
             <div className="logo"><img src={'../assets/Logo.png'} /></div>
-            <div className="menu">
-                <Link to="/">Home</Link>
-                <Link to="/team">Team</Link>
-                <Link to="/portfolio">Portfolio</Link>
-                <Link to="/services">Services</Link>
-                <Link to="/contact">Contact</Link>
+            <div className="menu-collapse" onClick={this.handleToggle}>
+                <div className="hamburger"></div>
+                <div className="hamburger"></div>
+                <div className="hamburger"></div>
             </div>
+            
         </header>
-    )
+        <div className={!!hamburger?"hamburger-menu animated bounceInDown":"menu"}>
+        <ul>
+                <li><i class="fab fa-houzz"></i><Link to="/">Home</Link></li>
+                <li><i class="fas fa-male"></i><Link to="/team">Team</Link></li>
+                <li><i class="fas fa-project-diagram"></i><Link to="/portfolio">Portfolio</Link></li>
+                <li><i class="fab fa-servicestack"></i><Link to="/services">Services</Link></li>
+                <li><i class="fab fa-contao"></i><Link to="/contact">Contact</Link></li>
+            </ul>
+            </div>
+            </div>
+        )
+    }
 }
 
 export default Header;
